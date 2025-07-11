52e1e0d2fae1d01832145f83dce5a8a95f70a3ec
nandakumarp-dev <nandakumarp.dev@gmail.com>
2025-07-11 14:14:19 +0000

    changed theme

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer() {
   return (
-    <footer className="text-center py-4 text-black ">
+    <footer className="text-center py-4 text-white ">
       <p>&copy; 2025 Nanda Kumar • Full Stack Developer Portfolio</p>
       <p>
-        <a href="#about" className="text-info me-3">About</a>
-        <a href="#projects" className="text-info me-3">Projects</a>
-        <a href="#contact" className="text-info">Contact</a>
+        <a href="#about" className="text-white me-3">About</a>
+        <a href="#projects" className="text-white me-3">Projects</a>
+        <a href="#contact" className="text-white">Contact</a>
       </p>
     </footer>
   );
